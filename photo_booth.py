e9844d02b881a7382b0a3cd984ab152230e9abae
thedarkman <snej@esuark.de>
2015-06-18 17:27:54 +0200

    checking status of printer rather than wait time

--- photo_booth.py
+++ photo_booth.py
@@ -4,8 +4,7 @@
 ## Get pslr-shoot from https://sourceforge.net/projects/pkremote/
 ## Just do "make cli" and "make install" after download as root on pi
 ##
-
-import RPi.GPIO as GPIO, datetime, time, os, subprocess
+import RPi.GPIO as GPIO, datetime, time, os, subprocess, atexit
 
 # GPIO setup
 GPIO.setmode(GPIO.BCM)
@@ -22,25 +21,35 @@ GPIO.setup(PRINT_LED, GPIO.OUT)
 GPIO.output(BUTTON_LED, True)
 GPIO.output(PRINT_LED, False)
 
+
+@atexit.register
+def cleanup():
+  GPIO.output(BUTTON_LED, False)
+  GPIO.output(POSE_LED, False)
+  GPIO.cleanup()
+
+def blinkPoseLed():
+  GPIO.output(POSE_LED, True)
+  time.sleep(1.5)
+  for i in range(5):
+    GPIO.output(POSE_LED, False)
+    time.sleep(0.4)
+    GPIO.output(POSE_LED, True)
+    time.sleep(0.4)
+  for i in range(5):
+    GPIO.output(POSE_LED, False)
+    time.sleep(0.1)
+    GPIO.output(POSE_LED, True)
+    time.sleep(0.1)
+  GPIO.output(POSE_LED, False) 
+
 while True:
   if (GPIO.input(SWITCH)):
     snap = 0
     while snap < 4:
       print("pose!")
       GPIO.output(BUTTON_LED, False)
-      GPIO.output(POSE_LED, True)
-      time.sleep(1.5)
-      for i in range(5):
-        GPIO.output(POSE_LED, False)
-        time.sleep(0.4)
-        GPIO.output(POSE_LED, True)
-        time.sleep(0.4)
-      for i in range(5):
-        GPIO.output(POSE_LED, False)
-        time.sleep(0.1)
-        GPIO.output(POSE_LED, True)
-        time.sleep(0.1)
-      GPIO.output(POSE_LED, False)
+      blinkPoseLed()
       print("SNAP")
       ## pslr-shoot does not support filename with date/time substitution, so concat manually
       d=datetime.datetime.now()
@@ -56,8 +65,13 @@ while True:
     subprocess.call("sudo /home/pi/scripts/photobooth/assemble_and_print", shell=True)
     # TODO: implement a reboot button
     # Wait to ensure that print queue doesn't pile up
-    # TODO: check status of printer instead of using this arbitrary wait time
-    time.sleep(110)
+    idle = False
+    while idle == False:
+       time.sleep(5)
+       statout = subprocess.check_output("lpstat -p", stderr=subprocess.STDOUT, shell=True)
+       if "idle" in statout:
+          idle = True
+          print("printer is ready")
     print("ready for next round")
     GPIO.output(PRINT_LED, False)
     GPIO.output(BUTTON_LED, True)
